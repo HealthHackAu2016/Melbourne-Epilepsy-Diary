698d64fb23164010b95e28cf01f41ee284648f23
Mike1Q84 <mike.zhang.monash@gmail.com>
2016-10-16 11:55:13 +1100

    Added median filter and 3rd order Butterworth filter

--- Server/tester/raw/plot.py
+++ Server/tester/raw/plot.py
@@ -3,7 +3,8 @@ import os, json
 #import pandas as pd
 from collections import OrderedDict
 
-path_to_json = '/var/www/html/healthhack/tester/raw/'
+#path_to_json = '/var/www/html/healthhack/tester/raw/'
+path_to_json = os.getcwd()
 json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
 
 timepoints = []
@@ -23,7 +24,7 @@ Zs = []
 #            Zs.append(data[timestamp]['Z'])
 import glob
 newest_json_filename = max(glob.iglob('*.json'), key=os.path.getctime)
-print(newest_json_filename)
+#print(newest_json_filename)
 
 #import datetime
 with open(newest_json_filename) as newest_json:
@@ -42,8 +43,8 @@ with open(newest_json_filename) as newest_json:
         Ys.append(data[timestamp]['Y'])
         Zs.append(data[timestamp]['Z'])
 
-import matplotlib
-matplotlib.use("Agg")
+#import matplotlib
+#matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 #import pyplot as plt
 #import matplotlib.dates as md
@@ -54,5 +55,34 @@ plt.plot(timepoints, Xs)
 plt.plot(timepoints, Ys)
 plt.plot(timepoints, Zs)
 #plt.show()
-plt.savefig('/var/www/html/healthhack/tester/raw/RawData.png')
+#plt.savefig('/var/www/html/healthhack/tester/raw/RawData.png')
+plt.savefig('RawData.png')
 plt.close()
+
+from scipy import signal
+XMeds = signal.medfilt(Xs)
+YMeds = signal.medfilt(Ys)
+ZMeds = signal.medfilt(Zs)
+plt.plot(timepoints, XMeds)
+plt.plot(timepoints, YMeds)
+plt.plot(timepoints, ZMeds)
+plt.savefig('MedData.png')
+plt.close()
+
+# Design Butterworth Filter
+# Filter Order
+N  = 3
+# Cutoff Frequency
+Wn = 0.5
+B, A = signal.butter(N, Wn, output='ba', analog=False)
+
+# Apply the Filter
+XButs = signal.filtfilt(B, A, XMeds)
+YButs = signal.filtfilt(B, A, YMeds)
+ZButs = signal.filtfilt(B, A, ZMeds)
+plt.plot(timepoints, XButs)
+plt.plot(timepoints, YButs)
+plt.plot(timepoints, ZButs)
+plt.savefig('ButData.png')
+plt.close()
+
